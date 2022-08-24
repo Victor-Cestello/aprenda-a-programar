e692c3bc1a8c84125a1bde4000d7dc8fe95a684f
Victor Cestello <vcestello@gmail.com>
2022-08-24 15:11:12 -0300

    desafio opcional

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -10,6 +10,5 @@ console.log(Profissão)
 console.log(Idade)
 console.log(local)
 
-
-
-
+console.log(`meu nome é ${Nome} e ${Profissão}`)
+console.log(`Olá, ${Nome} Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${local} que armazena o lugar que você sonha conhecer>. Confirma pra gente alguns dados? A sua idade é ${25}, você é ${Profissão} e o seu email é ${email} Ficamos no aguardo. Muito obrigado e boa viagem! :D}`)
